c520bfbc703d671c81fc0ae0a585c175b592c14d
Junko Tahara <junkotahara911@gmail.com>
2020-03-15 15:07:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   new_numbers = [ ]
   while counter < array.length do
-    new_numbers << array[counter] 
+    new_numbers << array[counter].
     counter += 1
   end
 end
